cfc3f2f52a449f10715fb7892d5a50b078559e90
Adrià <adria.lop.mar@gmail.com>
2021-06-07 11:26:18 +0200

    updated with fetch()

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 let pics =[];
+const api_url ='http://api.icndb.com/jokes/random' ;
 for(let i=1; i<11;i++){
     var pic = 'img-'+i+'.png'
     pics.push(pic)
 }
-function INSjoke(){
-    $.getJSON("http://api.icndb.com/jokes/random",
-    function (data) {
-        $('#picture').html('')
-        $('#joke').html(data.value.joke) 
-        let RNG = Math.floor(Math.random()*10)
-        $('#picture').append('<img class="rnd-img" src="images/'+pics[RNG]+'"/>')
-    });
-}
-$(document).ready( function(){      
-    INSjoke()
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const jokes = data;
+    document.getElementById('joke').textContent = jokes.value.joke
+    $('#picture').html('')
+    $('#joke').html(data.value.joke) 
+    let RNG = Math.floor(Math.random()*10)
+    $('#picture').append('<img class="rnd-img" src="images/'+pics[RNG]+'"/>')
+  }
+  $(document).ready( function(){ 
+    getISS();
     $('#boto').click(function(e){
         e.preventDefault();
-        INSjoke() 
+        getISS();   
     });
-});
+  });
